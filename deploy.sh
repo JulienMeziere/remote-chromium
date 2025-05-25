b7d06ad564ef1eec44dff73fb5565a3a3e2422d3
Julien MEZIERE <julien@trackit.io>
2025-05-25 22:46:42 +0200

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,9 @@ sshpass -p "$PASSWORD" ssh julien@54.37.8.207 bash << EOF
   cd ./remote-chromium &&
   git checkout . &&
   git pull &&
-  rm -rf /var/www/browser.meziere.org/* &&
+  echo "$PASSWORD" | sudo -S rm -rf /var/www/browser.meziere.org/* &&
   echo "$PASSWORD" | sudo -S cp -r ~/remote-chromium/noVNC/* /var/www/browser.meziere.org/ &&
-  echo "$PASSWORD" | sudo -S systemctl restart remote-chromium
-  echo "$PASSWORD" | sudo -S systemctl restart apache2
+  echo "$PASSWORD" | sudo mv /var/www/browser.meziere.org/vnc.html /var/www/browser.meziere.org/index.html &&
+  echo "$PASSWORD" | sudo -S systemctl restart remote-chromium &&
+  echo "$PASSWORD" | sudo -S systemctl reload apache2
 EOF
